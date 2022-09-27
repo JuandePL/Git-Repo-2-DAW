f2c008f9ba78211d6cf43084be24181327d86e4d
JuandePL <juandediosprunalopez@gmail.com>
2022-09-27 11:12:11 +0200

    boton-1 ejemplo dwec

--- DWEC/act_propuesta_1.7.1/js/script.js
+++ DWEC/act_propuesta_1.7.1/js/script.js
@@ -1,3 +1,5 @@
 function changeContent() {
     document.getElementById('prueba').innerHTML = 'CAMBIANDO el contenido'
 }
+
+document.getElementById('boton-1').addEventListener("click", changeContent)
