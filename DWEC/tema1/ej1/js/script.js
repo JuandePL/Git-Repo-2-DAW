1381227c691113c805eb3818b060d5bb14acbd1a
JuandePL <juandediosprunalopez@gmail.com>
2022-10-03 14:41:46 +0200

    refactorr ejercicio boton contador

--- DWEC/tema1/ej1/js/script.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById('add-button');
-const numberText = document.getElementById('number');
-let number = 1;
-
-button.addEventListener('click', () => {
-    numberText.innerHTML = number++;
-})
