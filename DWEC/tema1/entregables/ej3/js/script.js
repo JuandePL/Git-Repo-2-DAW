8869b41bf794517a0527a64e7cc0f047b0ea44a7
JuandePL <juandediosprunalopez@gmail.com>
2022-10-04 08:42:07 +0200

    tema1 ej3 dwec

--- DWEC/tema1/entregables/ej3/js/script.js
+++ DWEC/tema1/entregables/ej3/js/script.js
@@ -1,55 +1,18 @@
-const pregunta1 = document.getElementById('pregunta1')
-document.getElementById('verdadero1').onclick = () => {
-    pregunta1.style.color = "green"
-}
-document.getElementById('falso1').onclick = () => {
-    pregunta1.style.color = "red"
+const correctButtons = document.getElementsByClassName('correctButton')
+const wrongButtons = document.getElementsByClassName('wrongButton')
+
+function changeColor(questionId, color) {
+    document.getElementById(questionId).style.color = color
 }
 
-const pregunta2 = document.getElementById('pregunta2')
-document.getElementById('verdadero2').onclick = () => {
-    pregunta2.style.color = "red"
-}
-document.getElementById('falso2').onclick = () => {
-    pregunta2.style.color = "green"
+for (let i = 0; i < correctButtons.length; i++) {
+    correctButtons[i].onclick = () => {
+        changeColor(correctButtons[i].value, "green")
+    }
 }
 
-const pregunta3 = document.getElementById('pregunta3')
-document.getElementById('verdadero3').onclick = () => {
-    pregunta3.style.color = "red"
-}
-document.getElementById('falso3').onclick = () => {
-    pregunta3.style.color = "green"
-}
-
-const pregunta4 = document.getElementById('pregunta4')
-document.getElementById('verdadero4').onclick = () => {
-    pregunta4.style.color = "green"
-}
-document.getElementById('falso4').onclick = () => {
-    pregunta4.style.color = "red"
-}
-
-const pregunta5 = document.getElementById('pregunta5')
-document.getElementById('verdadero5').onclick = () => {
-    pregunta5.style.color = "red"
-}
-document.getElementById('falso5').onclick = () => {
-    pregunta5.style.color = "green"
-}
-
-const pregunta6 = document.getElementById('pregunta6')
-document.getElementById('verdadero6').onclick = () => {
-    pregunta6.style.color = "red"
-}
-document.getElementById('falso6').onclick = () => {
-    pregunta6.style.color = "green"
-}
-
-const pregunta7 = document.getElementById('pregunta7')
-document.getElementById('verdadero7').onclick = () => {
-    pregunta7.style.color = "green"
-}
-document.getElementById('falso7').onclick = () => {
-    pregunta7.style.color = "red"
+for (let i = 0; i < wrongButtons.length; i++) {
+    wrongButtons[i].onclick = () => {
+        changeColor(correctButtons[i].value, "red")
+    }
 }
