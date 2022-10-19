9b9b2272b958b71f1242d93cdacb1d59a9a854e3
JuandePL <juandediosprunalopez@gmail.com>
2022-10-19 10:17:29 +0200

    dwes ampliacion y loginejemplo

--- DWES/ejercicios2/ampliacion/toolsArray.php
+++ DWES/ejercicios2/ampliacion/toolsArray.php
@@ -22,7 +22,7 @@ function valueExists($array, $value) {
 function newArrayAndAddValueIfNotExists($size, $newValue = null) {
     $array = generateArray($size); // Rellenar array
 
-    // Comprobar si existe el nuevo valor e insertar valor si no existeç
+    // Comprobar si existe el nuevo valor e insertar valor si no existe
     $valuePositions = valueExists($array, $newValue);
     if ($valuePositions) {
         echo "El valor existe, está en las siguientes posiciones: " . "[" . implode(", ", $valuePositions) . "]<br>";
@@ -30,17 +30,21 @@ function newArrayAndAddValueIfNotExists($size, $newValue = null) {
         echo "No existe el nuevo valor, añadiendo...<br>";
         array_push($array, $newValue);
     }
-
+    
     return $array;
 }
 
 function newArrayAndAddValueToBeggining($size, $newValue) {
     $array = generateArray($size); // Rellenar
-
+    
     // Comprobar si existe el nuevo valor e insertar valor al inicio si no existe
-    if (!valueExists($array, $newValue)) {
+    $valuePositions = valueExists($array, $newValue);
+    if ($valuePositions) {
+        echo "El valor existe, está en las siguientes posiciones: " . "[" . implode(", ", $valuePositions) . "]<br>";
+    } else {
         echo "No existe el nuevo valor, añadiendo...<br>";
         array_unshift($array, $newValue); // Introducir valor al inicio
     }
+
     return $array;
 }
---
+++ DWES/ejercicios3/LoginEjemplo/login.php
@@ -0,0 +1,17 @@
+<?php
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$age = $_POST['age'];
+$password = $_POST['password'];
+
+echo "Nombre: $name<br>";
+echo "Apellidos: $surname<br>";
+echo "Edad: $age<br>";
+echo "Contraseña: $password<br>";
+
+if ($name == 'usuario' && $password == '1234') {
+    setcookie("login", true, time() + 6000);
+} else {
+    setcookie("login", false, time() + 6000);
+}
+?>
