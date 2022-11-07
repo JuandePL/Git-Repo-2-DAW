8f27637af2ccde57ac3de22eb01592960ff99b84
JuandePL <juandediosprunalopez@gmail.com>
2022-11-07 14:30:10 +0100

    dwec entregables4 terminado

--- DWEC/tema4/entregables/ej5/js/script.js
+++ DWEC/tema4/entregables/ej5/js/script.js
@@ -1,6 +1,8 @@
 const poblacion = prompt("Por favor introduzca una población")
 const codigopostal = prompt(`Ahora introduzca el código postal de ${poblacion}`)
 
+// Comprobamos si los datos estan rellenados correctamente.
+// Si alguno falla, se lo hacemos saber al usuariro
 document.getElementById('result').innerHTML = poblacion.trim() != "" || codigopostal.trim() != ""
     ? `El código postal de ${poblacion} es: ${codigopostal}`
     : "Los datos no han sido rellenados correctamente."
