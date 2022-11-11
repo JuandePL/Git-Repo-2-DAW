9303d2b349b92e508a2a09c7d1473461d4710d6c
JuandePL <juandediosprunalopez@gmail.com>
2022-11-11 14:41:35 +0100

    yoksetio

--- HLC/Factory de los huevos/main.py
+++ HLC/Factory de los huevos/main.py
@@ -1,14 +1,12 @@
-# import networkx as nx
+import networkx as nx
 
-# Grafo = nx.Graph()
-# Grafo.add_node("A")      # add_node("n") agrega "n" como nodo del grafo
-# Grafo.add_node("E")
-# Grafo.add_node("I")
-# Grafo.add_edge("A","E")  # add_edge("e") agrega "e" como borde de un nodo
-# Grafo.add_edge("E","I")
-# Grafo.add_edge("I","U")
+Grafo = nx.Graph()
+Grafo.add_node("A")      # add_node("n") agrega "n" como nodo del grafo
+Grafo.add_node("E")
+Grafo.add_node("I")
+Grafo.add_edge("A","E")  # add_edge("e") agrega "e" como borde de un nodo
+Grafo.add_edge("E","I")
+Grafo.add_edge("I","U")
 
-num = 1
-while True:
-    print(f'esta asignatura es de coña: parte {num}')
-    num *= 2
+# nx.draw(Grafo, 5, 3)
+print(nx.shortest_path(Grafo, "A", "I"))
