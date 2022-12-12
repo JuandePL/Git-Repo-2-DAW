4c8d84bb9fe1c8d84579b1e6299dbaa8574ff54f
JuandePL <juandediosprunalopez@gmail.com>
2022-12-12 12:32:36 +0100

    dwec cv eliminar node_modules

--- DWEC/tema5/examen_cv/view/templates/header.php
+++ DWEC/tema5/examen_cv/view/templates/header.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
 
 <?php include_once('../../model/DB.php') ?>
 
